9812422aed4360ef87857bd7f0288f6aec11fb24
Prakhar Gupta <prakhargupta3510@gmail.com>
2025-05-04 18:52:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+let cursor = document.querySelector("#cursor");
+let cursorBlur = document.querySelector("#cursor-blur");
+
+document.addEventListener("mousemove", function (e) {
+    cursor.style.left = e.clientX + "px";
+    cursor.style.top = e.clientY + "px";
+
+    cursorBlurblur.style.left = e.clientX + "px";
+    cursorBlur.style.top = e.clientY + "px";
+});
+
+
+
+gsap.to("#nav",{
+    backgroundColor:"#000",
+    height:"110px",
+    duration:0.5,
+    scrollTrigger:{
+        trigger:"#nav",
+        scroller:"body",
+        start:"top -10%",
+        end:"top -11%",
+        scrub:1
+    }
+})
+gsap.to("#main",{
+    backgroundColor:"#000",
+    scrollTrigger:{
+        trigger:"#main",
+        scroller:"body",
+        start:"top -25%",
+        end:"top -70%",
+        scrub:2
+    }
+})
